da697e6c6f15deb131a77db136dd6a7d8d740232
Feroz Syariff <feroz.syariff@fdmgroup.com>
2020-12-21 18:01:55 +0800

    Added new project

---
+++ jenkins-tutorial/src/main/java/com/jenkinstutorial/jenkins_tutorial/App.java
@@ -0,0 +1,13 @@
+package com.jenkinstutorial.jenkins_tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
